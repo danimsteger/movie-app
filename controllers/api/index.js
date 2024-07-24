efdefbb800efac70ea2ace27740c1a7fe15d5e36
Summer H <sh.hardison.sh@icloud.com>
2024-07-24 13:36:44 -0400

    Basis of routes setup

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+// const movieRoutes = require('./movieRoutes');
+// const reviewRoutes = require('./reviewRoutes');
+
+router.use('/users', userRoutes);
+// router.use('/movies', movieRoutes);
+// router.use('/reviews', reviewRoutes);
+
+module.exports = router;
