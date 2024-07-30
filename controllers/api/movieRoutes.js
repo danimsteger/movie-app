69807582c85fb8236db251ecb5e38eafb352ca2b
Kevin <kevhoang97@gmail.com>
2024-07-29 22:47:15 -0400

    add movie from the movies page

--- controllers/api/movieRoutes.js
+++ controllers/api/movieRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
     });
     res.status(200).json(movieData)
   } catch (err) {
-    res.status(500),json(err);
+    res.status(500).json(err);
   }
 });
 
--- public/js/addMovie.js
+++ public/js/addMovie.js
@@ -1,16 +1,43 @@
-const addMovie = async (event) => {
-  try {
-    console.log('step 2 worked!!');
-  } catch (err) {
-    console.error('Error in Adding Movie', err);
+const addMovieToProfile = async (event) => {
+  const imdbMovieId = event.target.getAttribute("data-imdbid");
+  const title = event.target.getAttribute("data-title");
+  const poster = event.target.getAttribute("data-poster");
+  const plot = event.target.getAttribute("data-plot");
+  const urls = decodeURIComponent(event.target.getAttribute("data-urls"));
+
+  if (!loggedIn) {
+    document.location.replace('/users/login');
+  } else {
+    try {
+      const response = await fetch("/api/movies", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          imdb_movieid: imdbMovieId,
+          title,
+          poster,
+          plot,
+          urls,
+        }),
+      });
+
+      if (response.ok) {
+        alert("Movie added to profile!");
+        document.location.replace('/users/movies');
+      } else if (response.status === 400) {
+        alert("Movie already exists in profile.");
+      } else {
+        throw new Error("Failed to add movie to profile");
+      }
+    } catch (error) {
+      console.error("Error adding movie to profile:", error);
+      alert("An error occurred while adding the movie to your profile.");
+    }
   }
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const addMovieButton = document.querySelector('.addButton');
-
-  if (addMovieButton) {
-    console.log('I clicked add movie button!');
-    addMovieButton.addEventListener('click', addMovie);
-  }
+document.querySelectorAll(".addButton").forEach(button => {
+  button.addEventListener("click", addMovieToProfile);
 });
